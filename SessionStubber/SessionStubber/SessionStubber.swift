87f4b12abf83a44a76e718b79e3e9905b1ea2cc0
Harmeet Singh <HarmeetSingh@paypoint.com>
2016-09-18 17:52:57 +0100

    master: Created project

---
+++ SessionStubber/SessionStubber/SessionStubber.swift
@@ -0,0 +1,10 @@
+//
+//  SessionStubber.swift
+//  SessionStubber
+//
+//  Created by Harmeet Singh on 18/09/2016.
+//  Copyright © 2016 HarmeetSingh. All rights reserved.
+//
+
+import Foundation
+
